d0ac36cd4313afe8290f5e6e644f0ebf247c8a44
SaiChaii <saichaitanyadudi@gmail.com>
2024-09-02 12:32:04 +0530

    commit after reset

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail';
+import WishList from './containers/WishList';
+import Login from './containers/Login';
+import Signup from './containers/Signup';
+import Mens from './containers/Mens';
+import Womens from './containers/Womens';
+import Groceries from './containers/Groceries';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import Home from './containers/Home';
+const LazyMens = React.lazy(() => import('./containers/Mens'));
+const LazyWomens = React.lazy(() => import('./containers/Womens'));
+const LazyKids = React.lazy(() => import('./containers/Kids'));
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <div className="content-container">
+          <Switch style={{ marginTop: '10000px' }}>
+            <Route path="/home" component={Home} />
+            <Route path="/allproducts" component={ProductListing} />
+            <Route path="/product/:productId" component={ProductDetail} />
+            <Route path="/wishlist" component={WishList} />
+            <Route path="/login" component={Login} />
+            <Route path="/sign-up" component={Signup} />
+            <Route path="/groceries" component={Groceries}/>
+            <Route path="/mens">
+              <React.Suspense fallback="Loading....">
+                <LazyMens />
+              </React.Suspense>
+            </Route>
+            <Route path="/women">
+              <React.Suspense fallback="Loading...">
+                <LazyWomens />
+              </React.Suspense>
+            </Route>
+            <Route path="/kids">
+              <React.Suspense fallback="Loading...">
+                <LazyKids />
+              </React.Suspense>
+            </Route>
+            <Route>404 Not Found</Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/ProductListing.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setProducts } from '../redux/actions/productActions';
+import ProductComponent from './ProductComponent';
+import { fetchProducts } from '../redux/actions/productActions';
+
+const ProductListing = () => {
+  const dispatch = useDispatch();
+
+  
+  useEffect(() => { 
+    dispatch(fetchProducts());
+  }, []);
+
+  //console.log("ProductListing After");
+
+  const renderall = () => {
+    return (
+      <div className="ui grid container">
+        <ProductComponent name="allproducts"/>
+      </div>
+    );
+  };
+  return <>{renderall()}</>;
+
+  // return (
+  //   <div className="ui grid container">
+  //     <ProductComponent name="allproducts"/>
+  //   </div>
+  // );
+};
+
+export default ProductListing;
